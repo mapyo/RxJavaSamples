b6ebb9c19a843580dcb6027054c74d283df906f0
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2017-02-10 02:20:50 +0900

    Add

--- app/src/test/java/com/mapyo/rxjavasamples/RxExampleUnitTest.kt
+++ app/src/test/java/com/mapyo/rxjavasamples/RxExampleUnitTest.kt
@@ -2,8 +2,8 @@ package com.mapyo.rxjavasamples
 
 import io.reactivex.Observable
 import io.reactivex.observers.DisposableObserver
+import io.reactivex.schedulers.Schedulers
 import org.junit.Test
-
 
 
 /**
@@ -46,7 +46,9 @@ class RxExampleUnitTest {
             }
         }
 
-        observableGreeting.subscribe(object : DisposableObserver<String>(){
+        observableGreeting
+                .observeOn(Schedulers.computation())
+                .subscribe(object : DisposableObserver<String>(){
             override fun onError(e: Throwable?) {
                 e?.printStackTrace()
             }
@@ -61,5 +63,40 @@ class RxExampleUnitTest {
                 println(threadName + ":" + t)
             }
         })
+    }
+
+    @Test @Throws(Exception::class)
+    fun sample3() {
+        val list = mutableListOf<String>()
+        (1..4).mapTo(list) { "hoge" + it }
+
+        val unit = Observable.create<String> {
+            emitter ->
+            Thread.sleep(300L)
+            val hoge = list.removeAt(0)
+            println(hoge)
+            emitter.onNext(hoge)
+            emitter.onComplete()
+        }
+
+        unit.observeOn(Schedulers.computation())
+                .repeat(10)
+                .subscribe(object : DisposableObserver<String>() {
+                    override fun onError(e: Throwable?) {
+                        e?.printStackTrace()
+                        println("error!!!")
+                    }
+
+                    override fun onNext(t: String?) {
+                        val threadName = Thread.currentThread().name
+                        println(threadName + ":" + t)
+                    }
+
+                    override fun onComplete() {
+                        val threadName = Thread.currentThread().name
+                        println(threadName + "完了しました")
+                    }
+                })
+        Thread.sleep(1000L)
     }
 }
