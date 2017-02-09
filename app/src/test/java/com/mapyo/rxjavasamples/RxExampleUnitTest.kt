084ea6464fd98d4b89225d3daef1125d439f5996
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2017-02-09 23:28:47 +0900

    Rename

--- app/src/test/java/com/mapyo/rxjavasamples/RxExampleUnitTest.kt
+++ app/src/test/java/com/mapyo/rxjavasamples/RxExampleUnitTest.kt
@@ -1,15 +1,14 @@
 package com.mapyo.rxjavasamples
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
 
  * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-class ExampleUnitTest {
+class RxExampleUnitTest {
     @Test @Throws(Exception::class)
     fun addition_isCorrect() {
         assertEquals(4, (2 + 2).toLong())
