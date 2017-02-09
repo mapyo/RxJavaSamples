46144ddbffeb42af5d94844fa8797709e4c79f2a
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2017-02-10 00:38:37 +0900

    Add

--- app/src/test/java/com/mapyo/rxjavasamples/RxExampleUnitTest.kt
+++ app/src/test/java/com/mapyo/rxjavasamples/RxExampleUnitTest.kt
@@ -4,6 +4,8 @@ import io.reactivex.Observable
 import io.reactivex.observers.DisposableObserver
 import org.junit.Test
 
+
+
 /**
  * Example local unit test, which will execute on the development machine (host).
 
@@ -26,5 +28,30 @@ class RxExampleUnitTest {
                         println(t)
                     }
                 })
+    }
+
+    @Test @Throws(Exception::class)
+    fun sample2() {
+        val observableGreeting : Observable<String> = Observable.create {
+            emitter ->
+            emitter.onNext("Hello, World!")
+            emitter.onNext("こんにちは、世界！")
+        }
+
+        observableGreeting.subscribe(object : DisposableObserver<String>(){
+            override fun onError(e: Throwable?) {
+                e?.printStackTrace()
+            }
+
+            override fun onComplete() {
+                val threadName = Thread.currentThread().name
+                println(threadName + "完了しました")
+            }
+
+            override fun onNext(t: String?) {
+                val threadName = Thread.currentThread().name
+                println(threadName + ":" + t)
+            }
+        })
     }
 }
