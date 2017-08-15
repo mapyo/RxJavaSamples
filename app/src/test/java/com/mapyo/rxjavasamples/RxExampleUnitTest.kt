10f940cec737c16ba8d1320180761dc270f043d4
mapyo <falcon.phantom.spidar.haptic@gmail.com>
2017-08-16 07:56:53 +0900

    Add sample

--- app/src/test/java/com/mapyo/rxjavasamples/RxExampleUnitTest.kt
+++ app/src/test/java/com/mapyo/rxjavasamples/RxExampleUnitTest.kt
@@ -536,6 +536,28 @@ class RxExampleUnitTest {
         subject.onNext(3)
     }
 
+    @Test @Throws(Exception::class)
+    fun sample_publish_onError() {
+        val subject = PublishSubject.create<Int>()
+
+        subject.subscribe ({ number ->
+            println(number.toString())
+
+            if (number == 3) throw RuntimeException()
+        }, {
+            println("onError")
+        }, {
+            println("onComplete")
+        })
+
+        subject.onNext(1)
+        subject.onNext(2)
+        subject.onNext(3)
+        subject.onNext(4)
+        subject.onNext(5)
+    }
+
+
     private fun getStringMutableList(count: Int): MutableList<String> {
         val list = mutableListOf<String>()
         (1..count).mapTo(list) { "hoge" + it }
